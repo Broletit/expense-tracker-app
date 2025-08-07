bab02001140c802b36fb9ce16667de8a9f452942
Broletit <thudao01102003@gmail.com>
2025-08-07 07:55:31 +0700

    fix postcss error!

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,15 @@ module.exports = {
   darkMode: 'class',
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/**/*.js", 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
   safelist: [
-  {
-    pattern: /(bg|text|ring)-(red|yellow|purple|pink|blue|green|indigo|gray)-(100|500|600)/,
-  },
-],
-
+    {
+      pattern: /(bg|text|ring)-(red|yellow|purple|pink|blue|green|indigo|gray)-(100|500|600)/,
+    },
+  ],
 }
-
